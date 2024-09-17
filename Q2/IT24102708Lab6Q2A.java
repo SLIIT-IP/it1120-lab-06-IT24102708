de61136a141a561bca7e1941751b1d18487e98c4
IT24102708 <it24102708@my.sliit.lk>
2024-09-17 20:56:53 +0530

    Add IT24102708Lab6Q2A.java to Q2

---
+++ Q2/IT24102708Lab6Q2A.java
@@ -0,0 +1,10 @@
+public class IT24102708Lab6Q2A {
+    public static void main(String[] args) {
+        int num = 1;
+
+        while (num <= 10) {
+            System.out.print(num + " ");
+            num++;
+        }
+    }
+}
